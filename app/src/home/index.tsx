a452cca584cc173001ec699ad543ffe8e1b5ae0e
刘小伟 <liuxw1@yonghui.cn>
2022-03-25 19:21:52 +0800

    feat: home

--- app/src/home/index.tsx
+++ app/src/home/index.tsx
@@ -4,21 +4,35 @@ import React from 'react';
 import styles from './index.module.less';
 import { CarouselRef } from 'antd/lib/carousel';
 
+interface Iprops {};
+interface Istate {
+  current: number;
+};
 
-class Home extends React.Component{
+class Home extends React.Component<Iprops,Istate>{
   carouselRef = React.createRef<CarouselRef>();
   constructor(porps: any){
     super(porps);
+    this.state={
+      current: 0
+    }
+  }
+  beforeChange=(f:number , t: number)=>{
+    console.log(f , t);
+    this.setState({
+      current: t
+    })
   }
   carouselItemClick=(item: number)=>{
     this.carouselRef?.current?.goTo(item)
   }
   renderCarouselItem=(type?: string)=>{
+    const { current } = this.state;
     const imageArray=['1' , '2' , '3' ,'4'];
     if(type === 'Thumbnail' ){
       return (
         imageArray.map((item , index)=><div onClick={this.carouselItemClick.bind(this , index)}>
-          <img style={{ width: 140 , height: 60, overflow: 'hidden' , marginBottom: 20 , cursor: 'pointer'}} src={require(`../../static/images/${item}.jpg`)} alt="" />
+          <img style={{ width: 140 , height: 60, overflow: 'hidden' , marginBottom: 20 , cursor: 'pointer', border: current === index? '1px solid #ff9d00': '0px'}} src={require(`../../static/images/${item}.jpg`)} alt="" />
         </div>)
       )
     }
@@ -34,7 +48,7 @@ class Home extends React.Component{
         <div className={styles.carouselThumbnail}>
           {this.renderCarouselItem('Thumbnail')}
         </div>
-        <Carousel ref={this.carouselRef} dotPosition={'right'} dots={false}>
+        <Carousel ref={this.carouselRef} dotPosition={'bottom'} dots={false} autoplay beforeChange={this.beforeChange}>
           {this.renderCarouselItem()}
       </Carousel>
       </div>
